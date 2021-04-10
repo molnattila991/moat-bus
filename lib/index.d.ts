dce2b103426730f3232a310a8ced2b6d1e053c2a
molnattila991 <molnarattila991@gmail.com>
2021-04-10 09:44:31 +0200

    -(edit) index.ts

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,8 +1,12 @@
 export declare const mfList: any;
+export declare const subs: any;
 export declare const addIFrame: (key: any) => void;
 export declare const removeIFrame: (key: any) => void;
 export declare const initBus: () => void;
+export declare const subscribeMaster: (eventKey: any, func: any) => void;
+export declare const unsubscribeMaster: (eventKey: string) => void;
+export declare const createBusEventMaster: (eventKey: string, data: any) => void;
 export declare const functions: any;
 export declare const init: () => void;
 export declare const subscribe: (name: any, func: any) => void;
-export declare const createBusEvent: (eventKey: any, data?: string) => void;
+export declare const createBusEvent: (eventKey: any, data?: any) => void;
